11381e4c32f03d24e1c08f435fa6f053062b02d4
vietduong906@gmail.com <56933370+DQuocViet@users.noreply.github.com>
2022-05-13 17:07:24 +0700

    update database

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from user.models import User, Location, UserProfile, RealEstateInfor
+from user.models import User, Address, UserProfile, RealEstateInfor
 
 admin.site.register(User)
-admin.site.register(Location)
+admin.site.register(Address)
 admin.site.register(UserProfile)
 admin.site.register(RealEstateInfor)
 
--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 # Create your models here.
-from system.models import District, Ward
+from system.models import District, Ward, Country, ProvinceCity
 
 
 class User(AbstractUser):
@@ -25,26 +25,28 @@ class User(AbstractUser):
         db_table = 'user'
 
 
-class Location(models.Model):
+class Address(models.Model):
+    country_id = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    province_city_id = models.ForeignKey(ProvinceCity, on_delete=models.SET_NULL, null=True)
     district_id = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
     ward_id = models.ForeignKey(Ward, on_delete=models.SET_NULL, null=True)
     name_street = models.CharField(max_length=64)
     number = models.CharField(max_length=5)
-    full_location = models.CharField(max_length=256)
+    full_address = models.CharField(max_length=256)
 
     def save(self, *args, **kwargs):
         self.full_location = self.number + ' ' + self.name_street + ',' + self.ward_id.name + ',' + self.district_id.name
-        super(Location, self).save(*args, **kwargs)
+        super(Address, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.full_location
 
     class Meta:
-        db_table = 'location'
+        db_table = 'address'
 
 
 class UserProfile(models.Model):
-    profile_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
     number_phone = models.CharField(max_length=14)
     email = models.EmailField()
@@ -60,7 +62,7 @@ class RealEstateInfor(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     type = models.CharField(max_length=64)
-    location_id = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    address_id = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
     create_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
